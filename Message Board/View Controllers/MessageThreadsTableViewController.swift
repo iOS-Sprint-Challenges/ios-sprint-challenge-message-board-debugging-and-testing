4bdee464836d3bac93ad244997ce1eb4d77ee34e
fritzgt <tuanis@live.com>
2020-08-14 12:33:42 +0300

    fixed bug where we called reload table in a background thread instead of the main thread

--- Message Board/View Controllers/MessageThreadsTableViewController.swift
+++ Message Board/View Controllers/MessageThreadsTableViewController.swift
@@ -37,7 +37,9 @@ class MessageThreadsTableViewController: UITableViewController {
         threadTitleTextField.text = ""
         
         messageThreadController.createMessageThread(with: threadTitle) {
-            self.tableView.reloadData()
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
         }
     }
     
